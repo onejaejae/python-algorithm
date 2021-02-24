adb8153af6a0758705eeda647b84ec92c73626c6
Cho Won Jae <yoteamo7@naver.com>
2021-02-24 21:15:53 +0900

    k번째 수

---
+++ 코드 구현력 기르기/k번째 수.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+# sys.stdin = open("c:\workspaces\python\코드 구현력 기르기\input.txt", "rt")
+
+T = int(input())
+for t in range(T):
+    i = 1
+    n, s, e, k = map(int, input().split())
+    arr = list(map(int, input().split()))
+    arr = arr[s-1:e]
+    arr.sort()
+
+    print("#%d %d" % (t+1, arr[k-1]))
