b2885e55d373dd365c332a6f7bbc5b376125b773
Cho Won Jae <yoteamo7@naver.com>
2021-03-10 00:15:10 +0900

    대표값 구하기

--- 코드 구현력 기르기/대표값.py
+++ 코드 구현력 기르기/대표값.py
@@ -1,14 +1,23 @@
 import sys
 import os
 
-sys.stdin = open("c:\workspaces\python\코드 구현력 기르기\input.txt", "rt")
+#sys.stdin = open("c:\workspaces\python\코드 구현력 기르기\input.txt", "rt")
 
 n = int(input())
 arr = list(map(int, input().split()))
+ave = round(sum(arr)/n)
+res = 0
+min = float('inf')
+for idx, x in enumerate(arr):
+    tmp = abs(ave - x)
+    if tmp < min:
+        min = tmp
+        score = x
+        res = idx+1
+    elif tmp == min:
+        if(score < x):
+            # min은 같으므로 안바꿔도 됨
+            score = x
+            res = idx+1
 
-sum = 0
-for x in arr:
-    sum += x
-
-ave = round(sum/n, 1)
-print(ave)
+print(ave, res)
