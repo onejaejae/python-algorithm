81fccb98529a46abeef08e6fce978ad135f017bc
Cho Won Jae <yoteamo7@naver.com>
2021-04-05 00:00:50 +0900

    씨름선수 (그리드)

--- 이분탐색(결정알고리즘) & 그리디 알고리즘/씨름선수(그리디).py
+++ 이분탐색(결정알고리즘) & 그리디 알고리즘/씨름선수(그리디).py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-# sys.stdin = open(
-#    "c:\workspaces\python\이분탐색(결정알고리즘) & 그리디 알고리즘\input.txt", "rt")
+sys.stdin = open(
+    "c:\workspaces\python\이분탐색(결정알고리즘) & 그리디 알고리즘\input.txt", "rt")
 
 n = int(input())
 arr = []
@@ -13,12 +13,12 @@ for _ in range(n):
 
 arr.sort(reverse=True)
 
-cnt = 1
-for i in range(1, n):
-    for j in range(i):
-        if arr[i][1] < arr[j][1]:
-            break
-    else:
+cnt = 0
+largest = 0
+
+for x, y in arr:
+    if y > largest:
+        largest = y
         cnt += 1
 
 print(cnt)
