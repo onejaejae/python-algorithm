37ad5a0ab736fa2bf723d43d189908eca5adcf2d
Cho Won Jae <yoteamo7@naver.com>
2021-04-15 17:54:31 +0900

    교육과정 설계(큐)

---
+++ 자료구조 활용 (스택, 큐, 해쉬, 힙)/교육과정 설계(큐).py
@@ -0,0 +1,25 @@
+import sys
+import os
+from collections import deque
+
+#sys.stdin = open("c:\workspaces\python\자료구조 활용 (스택, 큐, 해쉬, 힙)\input.txt", "rt")
+
+need = input()
+
+n = int(input())
+
+for i in range(n):
+    flag = True
+    dq = deque(need)
+    arr = list(input())
+
+    for j in range(len(arr)):
+        if any(arr[j] == x for x in dq):
+
+            if arr[j] != dq.popleft():
+                flag = False
+                break
+    if(flag and len(dq) == 0):
+        print("#%d YES" % (i+1))
+    else:
+        print("#%d NO" % (i+1))
