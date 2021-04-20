7502fbb856e77670285d9444cb1b609a48f04f2e
Cho Won Jae <yoteamo7@naver.com>
2021-04-21 00:27:06 +0900

    최대힙

--- 자료구조 활용 (스택, 큐, 해쉬, 힙)/최대힙.py
+++ 자료구조 활용 (스택, 큐, 해쉬, 힙)/최대힙.py
@@ -1,5 +1,19 @@
 import sys
 import os
-from collections import deque
+import heapq
 
-sys.stdin = open("c:\workspaces\python\자료구조 활용 (스택, 큐, 해쉬, 힙)\input.txt", "rt")
+#sys.stdin = open("c:\workspaces\python\자료구조 활용 (스택, 큐, 해쉬, 힙)\input.txt", "rt")
+
+maxHeap = []
+
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    if n == 0:
+        if len(maxHeap) == 0:
+            print(-1)
+        else:
+            print(heapq.heappop(maxHeap)[1])
+    else:
+        heapq.heappush(maxHeap, (-n, n))
