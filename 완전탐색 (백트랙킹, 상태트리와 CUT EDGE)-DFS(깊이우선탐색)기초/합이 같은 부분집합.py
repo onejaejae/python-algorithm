dab1ed15c7afc36f21350adeb3afbe1f4c93e650
Cho Won Jae <yoteamo7@naver.com>
2021-04-26 12:43:49 +0900

    합이 같은 부분집합(DFS)

--- 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/합이 같은 부분집합.py
+++ 완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초/합이 같은 부분집합.py
@@ -4,7 +4,21 @@ import os
 sys.stdin = open(
     "c:\workspaces\python\완전탐색 (백트랙킹, 상태트리와 CUT EDGE)-DFS(깊이우선탐색)기초\input.txt", "rt")
 
-n = int(input())
-arr = list(map(int, input().split()))
 
-total = sum(arr)
+def DFS(l, sum):
+    if l == n:
+        if sum == (total - sum):
+            print("YES")
+            sys.exit(0)
+    else:
+        DFS(l+1, sum + arr[l])
+        DFS(l+1, sum)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    total = sum(arr)
+    DFS(0, 0)
+    print("NO")
