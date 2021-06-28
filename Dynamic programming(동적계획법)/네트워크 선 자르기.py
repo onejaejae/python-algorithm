dfe2b9601eba1606589b054700b84fb4118ff052
Cho Won Jae <yoteamo7@naver.com>
2021-06-29 00:28:51 +0900

    네트워크 선 자르기(다이나믹)

--- Dynamic programming(동적계획법)/네트워크 선 자르기.py
+++ Dynamic programming(동적계획법)/네트워크 선 자르기.py
@@ -3,3 +3,14 @@ import os
 
 sys.stdin = open(
     "c:\workspaces\python\Dynamic programming(동적계획법)\input.txt", "rt")
+
+n = int(input())
+arr = [0] * n
+
+arr[0] = 1
+arr[1] = 2
+
+for i in range(2, n):
+    arr[i] = arr[i-2] + arr[i-1]
+
+print(arr[n-1])
