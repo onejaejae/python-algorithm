ffede57f47742b646e285750161305b52a0dfbdc
Cho Won Jae <yoteamo7@naver.com>
2021-07-31 14:21:27 +0900

    기능개발(큐)

---
+++ Programmers/기능개발.py
@@ -0,0 +1,25 @@
+
+
+def solution(progresses, speeds):
+
+    answer = []
+    time = 0
+    count = 0
+
+    while len(progresses) > 0:
+
+        if progresses[0] + (speeds[0] * time) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+
+    answer.append(count)
+    return answer
+
+
+print(solution([20, 99, 93, 30, 55, 10], [5, 10, 1, 1, 30, 5]))
