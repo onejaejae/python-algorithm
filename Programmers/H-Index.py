5a339677c1043091145cc2a174028a3cd86d31a7
Cho Won Jae <yoteamo7@naver.com>
2021-09-04 00:57:25 +0900

    H-Index

--- Programmers/H-Index.py
+++ Programmers/H-Index.py
@@ -1,3 +1,16 @@
 def solution(citations):
-    answer = 0
-    return answer
+
+    length = len(citations)
+    citations.sort()
+
+    for i in range(length, 0, -1):
+        for j in range(length):
+            if citations[j] >= i:
+                if length - j >= i and j <= i:
+                    return i
+                else:
+                    break
+    return 0
+
+
+print(solution([0, 1, 1]))
