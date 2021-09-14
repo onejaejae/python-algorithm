b19e5122f8e842357c0700c222c380f803431a5f
Cho Won Jae <yoteamo7@naver.com>
2021-09-14 23:09:34 +0900

    체육복 #수정

--- Programmers/체육복.py
+++ Programmers/체육복.py
@@ -1,16 +1,14 @@
 def solution(n, lost, reserve):
-
-    # set 차집합 => only resesrve
     set_reserve = set(reserve)-set(lost)
     set_lost = set(lost)-set(reserve)
-
     for i in set_reserve:
+        print(i)
         if i-1 in set_lost:
             set_lost.remove(i-1)
-        elif i + 1 in set_lost:
+        elif i+1 in set_lost:
             set_lost.remove(i+1)
 
-        return n - len(set_lost)
+    return n-len(set_lost)
 
 
 solution(5, [2, 4], [1, 3, 5])
